f67898167dfbcd3f49de646c064f51d1075aea22
ywh03 <yuwenhao03@gmail.com>
2024-02-03 23:26:23 +0800

    Add basic functionality to FollowedLeagues.jsx

--- client/src/components/FollowedLeagues.jsx
+++ client/src/components/FollowedLeagues.jsx
@@ -1,15 +1,43 @@
 import React from "react";
 import axios from "axios";
+import NavBar from "./NavBar.jsx";
 
 export default function FollowedLeagues () {
 
-    React.useEffect(() => {
+    const [allLeagues, setAllLeagues] = React.useState();
+    const [isLeaguesLoading, setLeaguesLoading] = React.useState(true);
 
+    async function getAllLeagues() {
+        const leagues = await axios.get('http://localhost:9000/leagues');
+        setAllLeagues(leagues.data);
+        setLeaguesLoading(false);
+    }
+
+    React.useEffect(() => {
+        getAllLeagues();
     }, [])
 
     return (
         <div>
-
+            <NavBar />
+            {
+                isLeaguesLoading ? (
+                    <p>Leagues Loading...</p>
+                ) : (
+                    <div>
+                        {
+                            allLeagues.map(function(league) {
+                                return (
+                                    <div>
+                                        <p>{league.alias}</p>
+                                        <p>{league.followStatus}</p>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                )
+            }
         </div>
     )
 }
